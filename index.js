53173bb297e39a2d370054194cd98b40a10f4a6d
maryemelamri <meriammeri.mm@gmail.com>
2023-08-02 16:17:41 +0100

    back research

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+// const openai =require("openai");
 const {Configuration, OpenAIApi} = require("openai");
 const config = new Configuration({
-   apiKey:"",
+   apiKey:"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",
    //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
 })
 const openai = new OpenAIApi(config);
  //setup Server
- const app = express ();
- app.use(bodyParser.json())
+const app = express ();
+app.use(bodyParser.json())
 app.use(cors());
 
 //endpoint for chatGpt
-app.post("/chat", async (req, res)=>{
-   const { prompt } = req.body;
-   const completion = await openai.createCompletion({
-      model:"text-davinci-003",
-      max_tokens: 512,
-      temperature:0,
-      prompt: prompt,
-      });
-      res.send(completion.data.choices[0].text);
-}) 
+// app.post("/chat", async (req, res)=>{
+//    const { prompt } = req.body;
+//    const completion = await openai.createCompletion({
+//       model:"text-davinci-003",
+//       max_tokens: 10,
+//       temperature:0,
+//       prompt: prompt,
+//       });
+//       res.send(completion.data.choices[0].text);
+// }) 
 
-const port =8080;
+
+//endpoint for grammerchecker
+app.post("/grammar-check", async (req, res)=>{
+   const {userPrompt} = req.body.textToCorrect;
+    //call the openAi API
+    const response = await openai.createCompletion({
+      model:"text-davinci-003",
+      prompt: `
+      You are a grammar checker. Correct the following text:
+      ${userPrompt}
+    `,      max_tokens:50,
+      temperature:0.7,
+      stop:'/n'
+    });
+   console.log("response"+response)
+  const correctedText = response.data.choices[0].text.trim();
+  res.json({ correctedText });
+  console.log('teeesting'+ res)
+});
+const port = 8080;
 app.listen(port, ()=>{
    console.log(`server listening on port ${port}`)
-
 })
+

10265315d98cbf8b7e8ae97b59312658aa4a7bff
maryemelamri <meriammeri.mm@gmail.com>
2023-08-09 22:38:18 +0100

    plag server error

--- index.js
+++ index.js
@@ -1,51 +1,88 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-// const openai =require("openai");
-const {Configuration, OpenAIApi} = require("openai");
-const config = new Configuration({
-   apiKey:"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",
-   //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
-})
-const openai = new OpenAIApi(config);
- //setup Server
-const app = express ();
-app.use(bodyParser.json())
-app.use(cors());
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-//endpoint for chatGpt
-// app.post("/chat", async (req, res)=>{
-//    const { prompt } = req.body;
-//    const completion = await openai.createCompletion({
-//       model:"text-davinci-003",
-//       max_tokens: 10,
-//       temperature:0,
-//       prompt: prompt,
-//       });
-//       res.send(completion.data.choices[0].text);
-// }) 
+const app = express();
+app.use(bodyParser.json());
 
 
-//endpoint for grammerchecker
-app.post("/grammar-check", async (req, res)=>{
-   const {userPrompt} = req.body.textToCorrect;
-    //call the openAi API
-    const response = await openai.createCompletion({
-      model:"text-davinci-003",
-      prompt: `
-      You are a grammar checker. Correct the following text:
-      ${userPrompt}
-    `,      max_tokens:50,
-      temperature:0.7,
-      stop:'/n'
-    });
-   console.log("response"+response)
-  const correctedText = response.data.choices[0].text.trim();
-  res.json({ correctedText });
-  console.log('teeesting'+ res)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-const port = 8080;
-app.listen(port, ()=>{
-   console.log(`server listening on port ${port}`)
-})
 
+const makePlagiarismCheck = async () => {
+  const burp0_url = "https://papersowl.com:443/plagiarism-checker-send-data";
+
+  const burp0_cookies = {
+    PHPSESSID: "qjc72e3vvacbtn4jd1af1k5qn1",
+    first_interaction_user:"%7B%22referrer%22%3A%22https%3A%5C%2F%5C%2Fwww.google.com%5C%2F%22%2C%22internal_url%22%3A%22%5C%2Ffree-plagiarism-checker%22%2C%22utm_source%22%3Anull%2C%22utm_medium%22%3Anull%2C%22utm_campaign%22%3Anull%2C%22utm_content%22%3Anull%2C%22utm_term%22%3Anull%2C%22gclid%22%3Anull%2C%22msclkid%22%3Anull%2C%22adgroupid%22%3Anull%2C%22targetid%22%3Anull%2C%22appsflyer_id%22%3Anull%2C%22appsflyer_cuid%22%3Anull%2C%22cta_btn%22%3Anull%7D",
+    first_interaction_order: "%7B%22referrer%22%3A%22https%3A%5C%2F%5C%2Fwww.google.com%5C%2F%22%2C%22internal_url%22%3A%22%5C%2Ffree-plagiarism-checker%22%2C%22utm_source%22%3Anull%2C%22utm_medium%22%3Anull%2C%22utm_campaign%22%3Anull%2C%22utm_content%22%3Anull%2C%22utm_term%22%3Anull%2C%22gclid%22%3Anull%2C%22msclkid%22%3Anull%2C%22adgroupid%22%3Anull%2C%22targetid%22%3Anull%2C%22appsflyer_id%22%3Anull%2C%22appsflyer_cuid%22%3Anull%2C%22cta_btn%22%3Anull%7D", 
+    affiliate_user: "a%3A3%3A%7Bs%3A9%3A%22affiliate%22%3Bs%3A9%3A%22papersowl%22%3Bs%3A6%3A%22medium%22%3Bs%3A9%3A%22papersowl%22%3Bs%3A8%3A%22campaign%22%3Bs%3A9%3A%22papersowl%22%3B%7D", 
+    sbjs_migrations: "1418474375998%3D1", 
+    sbjs_current_add: "fd%3D2022-05-24%2019%3A01%3A22%7C%7C%7Cep%3Dhttps%3A%2F%2Fpapersowl.com%2Ffree-plagiarism-checker%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F",
+    sbjs_first_add: "fd%3D2022-05-24%2019%3A01%3A22%7C%7C%7Cep%3Dhttps%3A%2F%2Fpapersowl.com%2Ffree-plagiarism-checker%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F",
+    sbjs_current: "typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29", 
+    sbjs_first: "typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29", 
+    sbjs_udata: "vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Windows%20NT%206.3%3B%20Win64%3B%20x64%3B%20rv%3A100.0%29%20Gecko%2F20100101%20Firefox%2F100.0", 
+    sbjs_session: "pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fpapersowl.com%2Ffree-plagiarism-checker", 
+    _ga_788D7MTZB4: "GS1.1.1653411683.1.0.1653411683.0",
+    _ga: "GA1.1.1828699233.1653411683",
+    trustedsite_visit: "1",
+    trustedsite_tm_float_seen: "1",
+    AppleBannercookie_hide_header_banner: "1",
+    COOKIE_PLAGIARISM_CHECKER_TERMS: "1", 
+    plagiarism_checker_progress_state: "1"
+    // Add other required cookies here...
+  };
+  
+  const burp0_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
+    "Accept": "*/*", 
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3", 
+    "Accept-Encoding": "gzip, deflate",
+    "Referer": "https://papersowl.com/free-plagiarism-checker", 
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", 
+    "X-Requested-With": "XMLHttpRequest",
+    "Origin": "https://papersowl.com", 
+    "Dnt": "1", "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "no-cors", 
+    "Sec-Fetch-Site": "same-origin",
+    "Pragma": "no-cache", 
+    "Cache-Control": "no-cache", "Te": "trailers", 
+    "Connection": "close"
+   
+  };
+  const burp0_data = {
+    is_free: "false",
+    plagchecker_locale: "en",
+    product_paper_type: "1",
+    title: '',
+    text: "Eminem se met à attirer plus d'attention autour de lui en développant Slim Shady, un alter ego violent et sadique qui permet au rappeur d'exprimer sa colère avec des paroles traitant notamment de drogues, de viol et de meurtre"
+  };
+  try {
+
+    const response = await axios.post(burp0_url, burp0_data, {
+      headers: burp0_headers,
+    cookies: burp0_cookies, // axios does not directly support setting cookies, so you may need to use a cookie jar library for this
+      withCredentials: false, // This ensures that cookies are included in the request
+      responseType: 'json'
+    }); 
+
+    // Process the response data here
+    const r = response.data;
+    str=  JSON.stringify(r);
+    console.log(" Wttttttttttttttffffuck : " + str);
+     result = JSON.parse(str.text);
+  
+   
+    console.log("\n[!] Word count : " + result.words_count);
+    console.log("\n[!] Turnitin index : " + (100 - parseFloat(result.percent)));
+    console.log("\n[!] Matches : " + result.matches);
+  } catch (error) {
+    console.error('Error while checking plagiarism:', error);
+  }
+};
+
+// Call the function to make the plagiarism check
+makePlagiarismCheck();
